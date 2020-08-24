8ed3c576f973a4553ec601a81d18ea32e2045224
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-08-23 20:51:32 -0600

    Setup Backend

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+'use strict';
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const express = require('express');
+const bodyParser = require('body-parser');
+let cors = require('cors');
+const app = express();
+//const http = require('http').Server(app);
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+let bcrypt = require('bcryptjs');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'xxx.xxx.xxx.xxx',
+    user: '----',
+    password: '-----',
+    database: '----',
+    port: 3306
+});
+connection.connect();
+
+const s3 = new AWS.S3({
+    region: 'xxxxxx',
+    accessKeyId: 'xxxxxx',
+    secretAccessKey: 'xxxxxxx'
+});
+
+
+//----------------------------------------------------PRUEBA CONEXIÓN A DB------------------------------------------------
+app.get('/rol', (req, res) => {
+    connection.query('show databases', function (err, rows, fields) {
+        if (!err) {
+            res.send(rows)
+        }
+        else {
+            throw err;
+        }
+    });
+});
+
+// server init
+app.listen(3000, () => console.log('escuchando en puerto 3000'));
