11ef53d93ded3d072815355f7c6968330851a04c
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-08-26 12:02:59 -0600

    Se ha cambiado  la seguridad

--- backend/aws/aws_keys.js
+++ backend/aws/aws_keys.js
@@ -1,14 +1,8 @@
 let aws_keys ={
     s3:{
-<<<<<<< HEAD
         region:'xxxxxxxxxxxx',
         accessKeyId: "xxxxxxxxxxxxx", 
         secretAccessKey: "xxxxxxxxxxxxxxxxxxxx"
-=======
-        region:'us-east-2',
-        accessKeyId: "AKIAJMNX2XOJTWT5B6XQ", 
-        secretAccessKey: "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"
->>>>>>> b_crear_tarea
     }
 }
 
