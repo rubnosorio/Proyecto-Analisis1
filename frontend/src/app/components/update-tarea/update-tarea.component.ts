64d3414992ab0716a25d2659742a9f3165344b6d
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-08-25 19:12:19 -0600

    agregado campo fecha de entrega

--- frontend/src/app/components/update-tarea/update-tarea.component.ts
+++ frontend/src/app/components/update-tarea/update-tarea.component.ts
@@ -10,20 +10,25 @@ import { Router } from '@angular/router';
 
 export class UpdateTareaComponent implements OnInit {
 
-  idtarea: number=0;
+  idtarea: number;
   titulo: string;
-  instruccion: string;
+  descripcion: string;
+  fecha_entrega: string;
 
   constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.idtarea=0;
     this.titulo='Tarea N';
-    this.instruccion='Instrucciones Tarea N\n 1) Instruccion 1\n 2) Instruccion 2\n 3) Instruccion 3\n'
+    this.descripcion='Instrucciones Tarea N\n 1) Instruccion 1\n 2) Instruccion 2\n 3) Instruccion 3\n';
+    this.fecha_entrega='2020-08-30T23:59';
   }
 
   actualizar(){
-    if(this.titulo.length != 0 && this.instruccion.length != 0){
-      console.log(this.titulo+'\n '+this.instruccion);
+    if(this.titulo.length != 0 && this.descripcion.length != 0 && this.fecha_entrega.length != 0){
+      console.log('Titulo: '+this.titulo);
+      console.log('Descripción: '+this.descripcion);
+      console.log('Fecha Entrega: '+this.fecha_entrega);
     }else{
       console.log('No se puede actualizar tarea si esta vacia.');
     }
