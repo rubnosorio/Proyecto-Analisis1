f91952db23ec303f6ff47898825794362de33d59
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-08-26 19:43:47 -0600

    Se atualizó el campo archivo

--- backend/endpoints/crear_tarea.js
+++ backend/endpoints/crear_tarea.js
@@ -8,7 +8,7 @@ module.exports = (app, mysql, s3, connection) => {
         var id_clase = req.body.id_clase;
 
         //variables para uso de base64
-        var base64String = req.body.imagen; //variable que recibe la información en base64 del archivo
+        var base64String = req.body.archivo; //variable que recibe la información en base64 del archivo
         var extension = base64String.substring(base64String.indexOf('/') + 1, base64String.indexOf(';base64')); //obtiene la información de la extension a partir del header
         var base64 = base64String.split(","); //separamos el header del codigo en base64
         //variables para el bucket de S3
