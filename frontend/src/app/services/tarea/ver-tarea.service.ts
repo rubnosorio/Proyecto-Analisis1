b37189fe869a5506cb49d80ff5435da81d423f60
Henry Galvez <soulofhenry@gmail.com>
2020-08-28 17:21:23 -0600

    se añadio archivo baseURL

--- frontend/src/app/services/tarea/ver-tarea.service.ts
+++ frontend/src/app/services/tarea/ver-tarea.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseURL'
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerTareaService {
 
-  readonly URL_API = "http://localhost:3000/"
-
   constructor(private http: HttpClient) { }
 
   getTasks(idclase: number){
-    return this.http.post(this.URL_API+'ver_tareas',{id_clase: idclase})
+    return this.http.post(baseURL+'ver_tareas',{id_clase: idclase})
   }
 
 }
