b12ccbc80b068e3393ce146fca2d5a4298438db3
Henry Galvez <soulofhenry@gmail.com>
2020-09-01 20:59:31 -0600

    se añadio pruebas para vista ver examenes

--- frontend/src/app/components/ver-examenes/ver-examenes.component.spec.ts
+++ frontend/src/app/components/ver-examenes/ver-examenes.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { VerExamenesComponent } from './ver-examenes.component';
 
 describe('VerExamenesComponent', () => {
@@ -22,4 +22,17 @@ describe('VerExamenesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("obtenienodo los examenes", () => {
+    component.gets()
+    var array = component.examenes
+    expect(array.length).toBe(10)
+  })
+
+  it("examenes que se muestran al usuario", () => {
+    const tasks = fixture.debugElement.queryAll(By.css('mat-accordion mat-expansion-panel'));
+    expect(tasks.length).toBe(10);
+    
+  })
+
 });
--- frontend/src/app/models/entregar-tarea.spec.ts
+++ frontend/src/app/models/entregar-tarea.spec.ts
@@ -2,6 +2,6 @@ import { EntregarTarea } from './entregar-tarea';
 
 describe('EntregarTarea', () => {
   it('should create an instance', () => {
-    expect(new EntregarTarea()).toBeTruthy();
+    expect(new EntregarTarea(1,1,1,"","","",0)).toBeTruthy();
   });
 });
