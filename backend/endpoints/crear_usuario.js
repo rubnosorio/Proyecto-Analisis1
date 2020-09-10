30e9937e1f80a922eb3a5aa930f31cefa7eb5d66
Henry Galvez <soulofhenry@gmail.com>
2020-09-09 19:04:21 -0600

    prueba: verificando que reciba los parametros (exito)

--- backend/endpoints/crear_usuario.js
+++ backend/endpoints/crear_usuario.js
@@ -2,4 +2,9 @@ module.exports=(app,connection)=>{
     app.get('/crear_usuario',(req,res)=>{
         res.status(200).send()
     })
+
+    app.post('/crear_usuario',(req,res)=>{
+        console.log(req.body)
+        res.status(200).send(req.body)
+    })
 }
--- backend/spec/crearUsuarioSpec.js
+++ backend/spec/crearUsuarioSpec.js
@@ -10,4 +10,25 @@ fdescribe('Cuando se registra un usuario', function () {
             done();
         });
     });
+
+    it('deberia recibir los parametros', function (done) {
+        var usuario = {
+            id_usuario: 0,
+            nombres: "nombre prueba",
+            apellidos: "apellido prueba",
+            username: "username prueba",
+            passwd: "passwd prueba",
+            correo: "correo prueba",
+        }
+        request.get(endpoint, {json: true, body: {usuario}}, function (error, response) {
+            var temp = JSON.parse(response.request.body).usuario
+            expect(temp.id_usuario).toEqual(usuario.id_usuario);
+            expect(temp.nombres).toEqual(usuario.nombres);
+            expect(temp.apellidos).toEqual(usuario.apellidos);
+            expect(temp.username).toEqual(usuario.username);
+            expect(temp.passwd).toEqual(usuario.passwd);
+            expect(temp.correo).toEqual(usuario.correo);
+            done();
+        });
+    });
 });
