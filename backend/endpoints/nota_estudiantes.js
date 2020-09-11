2ecda8677467047cbcba84e10df9e22c27edbd36
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-11 16:01:43 -0600

    Prueba de nota estudiante para las tareas refactorizada, (prueba aceptada)

--- backend/endpoints/nota_estudiantes.js
+++ backend/endpoints/nota_estudiantes.js
@@ -16,6 +16,55 @@ module.exports = (app,connection) => {
                 var response={
                     lista_notas:[]
                 };
+                //buscamos nombres diferentes y los guardamos en una lista auxiliar
+                var lista_nombres=[];
+                for(let i=0;i<rows.length;i++){
+                    var estudiante={
+                        id_estudiante:rows[i].id_usuario,
+                        nombres: rows[i].nombres,
+                        apellidos:rows[i].apellidos
+                    }
+                    if(i==0){
+                        lista_nombres.push(estudiante);
+                    }
+                    else{
+                        var existe=false;
+                        for(let j=0;j<lista_nombres.length;j++){
+                            if(lista_nombres[j].id_estudiante==estudiante.id_estudiante){
+                                existe=true
+                                break;
+                            }
+                        }
+                        if(existe==false){
+                            lista_nombres.push(estudiante);
+                        }
+                    }
+                }
+                //lista para almacenar a los estudiantes con sus notas
+                var lista_notas=[];
+                //buscamos las notas de las tareas y examenes de los estudiantes y los insertamos
+                for(let i=0;i<lista_nombres.length;i++){
+                    var estudiante_notas={
+                        id_estudiante:lista_nombres[i].id_estudiante,
+                        nombres:lista_nombres[i].nombres,
+                        apellidos:lista_nombres[i].apellidos,
+                        lista_tareas:[],
+                        lista_examenes:[]
+                    }
+                    //buscamos las notas de tareas dentro del resultado
+                    for(let j=0;j<rows.length;j++){
+                        if(rows[j].id_usuario==estudiante_notas.id_estudiante){
+                            var tarea={
+                                tarea:rows[j].nombre_tarea,
+                                nota:rows[j].nota
+                            }
+                            estudiante_notas.lista_tareas.push(tarea);
+                        }
+                    }
+                    lista_notas.push(estudiante_notas);
+                }
+                //console.log(lista_notas[0].lista_tareas);
+                response.lista_notas=lista_notas;
                 res.status(200).send(response);
             }
             else {
--- backend/spec/notaEstudiantesSpec.js
+++ backend/spec/notaEstudiantesSpec.js
@@ -7,6 +7,14 @@ describe('nota de estudiantes', function () {
     var clase={
         id_clase:1
     }
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+    });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
     it('debe devolver estado 200', function (done) {
         request.get(endpoint, { json: true, body: clase }, function (error, response) {
             expect(response.statusCode).toEqual(200);
@@ -15,7 +23,7 @@ describe('nota de estudiantes', function () {
     });
     it('debe devolver una lista no vacía o vacía',function(done){
         request.get(endpoint, { json: true, body: clase }, function (error, response) {
-            expect(response.body.lista_notas).toEqual([]||![]);
+            expect(response.body.lista_notas.length).toBeGreaterThanOrEqual(0);
             done();
         });
     });
@@ -24,7 +32,7 @@ describe('nota de estudiantes', function () {
             if(response.body.lista_notas.length>0){
                 var nota_tarea=0;
                 for(let i=0;i<response.body.lista_notas[0].lista_tareas.length;i++){
-                    nota_tarea+=response.body.lista_notas[0].lista_tareas[i];
+                    nota_tarea+=response.body.lista_notas[0].lista_tareas[i].nota;
                 }
                 expect(nota_tarea).toBeGreaterThanOrEqual(0);
                 done();
