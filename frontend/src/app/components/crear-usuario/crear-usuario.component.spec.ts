d1f736b7ec5c78239e8e2933dba74bea77b301a7
Henry Galvez <soulofhenry@gmail.com>
2020-09-10 19:26:23 -0600

    se añadio prueba para verificar el form control (fallo)

---
+++ frontend/src/app/components/crear-usuario/crear-usuario.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CrearUsuarioComponent } from './crear-usuario.component';
+fdescribe('CrearUsuarioComponent', () => {
+  let component: CrearUsuarioComponent;
+  let fixture: ComponentFixture<CrearUsuarioComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CrearUsuarioComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CrearUsuarioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Deberia existir un form group', () => {
+    expect(!component).toBeTruthy();
+  });
+});
