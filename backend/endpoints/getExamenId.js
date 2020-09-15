85a6cb166d340da6e6a64e54bf89b10f825b8f87
René Corona <3114327190409@ingenieria.usac.edu.gt>
2020-09-15 14:18:22 -0600

    Se pasaron 0 de 3 pruebas (Error al proveer el endpoint de la prueba)

--- backend/endpoints/getExamenId.js
+++ backend/endpoints/getExamenId.js
@@ -1,5 +1,26 @@
 module.exports = (app, connection) => {
     app.get('/get_exam_id', (req, res) => {
-        res.send('');
+        if (req.body.id_examen < 0) {
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({});
+        }
+        else if (req.body.id_examen == undefined || req.body.id_examen == '') {
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({});
+        }
+        else {
+            connection.query(`select * from EXAMEN where id_examen = ${req.body.id_examen}`, function (err, rows, fields) {
+                if (!err) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(rows);
+                }
+                else {
+                    throw err;
+                }
+            });
+        }
     });
 }
