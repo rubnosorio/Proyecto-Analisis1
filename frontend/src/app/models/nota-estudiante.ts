ddbe4ee5d87b468840e84761a5ed362f5d59e1a7
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-14 19:06:10 -0600

    Se actualizo la prueba al modelo para crear la nota de estudiantess (exitoso)

--- frontend/src/app/models/nota-estudiante.ts
+++ frontend/src/app/models/nota-estudiante.ts
@@ -1,2 +1,15 @@
+import { NotaTarea } from './nota-tarea'
+
 export class NotaEstudiante {
+    id_estudiante:number
+    nombres:string
+    apellidos:string
+    lista_tarea: NotaTarea[]
+
+    constructor(_id_estudiante:number, _nombres:string, _apellidos:string, _lista_tarea:NotaTarea[]){
+        this.id_estudiante=_id_estudiante;
+        this.nombres=_nombres;
+        this.apellidos=_apellidos;
+        this.lista_tarea=_lista_tarea;
+    }
 }
