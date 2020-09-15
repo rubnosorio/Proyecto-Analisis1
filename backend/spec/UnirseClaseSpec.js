1913dbbb72e9cc055fb7faaf120fc7e0e4212fc6
Carlos <manuelg.e99@gmail.com>
2020-09-15 13:31:22 -0600

    Se añadió prueba de conexión a base en unirse a clase

---
+++ backend/spec/UnirseClaseSpec.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const server = require('../index');
+
+const endpoint = 'http://localhost:3000/unirse_a_clase';
+
+describe('unirse_a_clase', function () {
+    
+    it('Verificar conexión status 200', function (done) {
+        request.post(endpoint, function (error, response) {
+            expect(response.statusCode1).toEqual(200);
+            done();
+        });
+    });
+
+    
+
+
+});
