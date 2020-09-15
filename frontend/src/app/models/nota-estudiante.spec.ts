2e9d5607396d89f9ed9071d2e56dbf9ecdd85ab7
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-14 18:52:25 -0600

    Se agregó la prueba al modelo para crear la nota de estudiantess (fallido)

---
+++ frontend/src/app/models/nota-estudiante.spec.ts
@@ -0,0 +1,7 @@
+import { NotaEstudiante } from './nota-estudiante';
+
+fdescribe('NotaEstudiante', () => {
+  it('Deberia crear un estudiante con sus notas', () => {
+    expect(new NotaEstudiante(1,'Erick','Lemus',[])).toBeTruthy();
+  });
+});
--- frontend/src/app/models/nota-tarea.spec.ts
+++ frontend/src/app/models/nota-tarea.spec.ts
@@ -1,6 +1,6 @@
 import { NotaTarea } from './nota-tarea';
 
-fdescribe('NotaTarea', () => {
+describe('NotaTarea', () => {
   it('debería crear una nueva tarea con los datos proporcionados', () => {
     expect(new NotaTarea(1,'tarea 1',25)).toBeTruthy();
   });
