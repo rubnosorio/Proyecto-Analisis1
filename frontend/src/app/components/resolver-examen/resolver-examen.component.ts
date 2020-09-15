bf49a0536bd74697bcfc0f49838a1f5197b425da
René Corona <3114327190409@ingenieria.usac.edu.gt>
2020-09-14 21:46:24 -0600

    Se finalizo el desarrollo (Se pasaron 5 de 5 pruebas)

--- frontend/src/app/components/resolver-examen/resolver-examen.component.ts
+++ frontend/src/app/components/resolver-examen/resolver-examen.component.ts
@@ -13,15 +13,18 @@ export class ResolverExamenComponent implements OnInit {
   examen: any;
   id_examen = -1;
 
+  respuestas: any = [];
+
   formtf: FormGroup;
   formarea: FormGroup;
 
   constructor(private route: ActivatedRoute, private fb: FormBuilder) {
     this.id_examen = Number(this.route.snapshot.paramMap.get('id'));
     this.examen = this.obtenerexamen(this.id_examen);
+    this.createForm();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   createForm(): void {
     this.formtf = this.fb.group({
@@ -60,7 +63,64 @@ export class ResolverExamenComponent implements OnInit {
   }
 
   calificar(): Number {
-    return -1;
+    if (this.examen.preguntas[this.indexactual].tipo_respuesta == 2) {
+      this.examen.preguntas[this.indexactual].res = this.formtf.controls[
+        'answer_res'
+      ].value;
+      this.formtf.reset();
+    } else if (this.examen.preguntas[this.indexactual].tipo_respuesta == 1) {
+      this.examen.preguntas[this.indexactual].res = this.formarea.controls[
+        'answer_res'
+      ].value;
+      this.formarea.reset();
+    }
+
+    var correctas = 0;
+
+    for (var i = 0; i < this.examen.preguntas.length; i++) {
+      switch (this.examen.preguntas[i].tipo_respuesta) {
+        case 1:
+          this.respuestas.push({ pregunta: this.examen.preguntas[i].pregunta, respuestas: this.examen.preguntas[i].res });
+          if (this.examen.preguntas[i].opciones[0].respuesta == this.examen.preguntas[i].res) {
+            correctas++;
+          }
+          break;
+        case 2:
+          this.respuestas.push({ pregunta: this.examen.preguntas[i].pregunta, respuesta: this.examen.preguntas[i].res });
+          if (this.examen.preguntas[i].res != undefined) {
+            for (var j = 0; j < this.examen.preguntas[i].opciones.length; j++) {
+              if (this.examen.preguntas[i].opciones[j].respuesta == 'Verdadero' && this.examen.preguntas[i].opciones[j].es_correcta == 1 && this.examen.preguntas[i].res == 1) {
+                correctas++;
+                break;
+              } else if (this.examen.preguntas[i].opciones[j].respuesta == 'Falso' && this.examen.preguntas[i].opciones[j].es_correcta == 1 && this.examen.preguntas[i].res == 0) {
+                correctas++;
+                break;
+              }
+            }
+          }
+          break;
+        case 0:
+          var internas = 0;
+          this.respuestas.push({ pregunta: this.examen.preguntas[i].pregunta, respuesta: [] });
+          for (var j = 0; j < this.examen.preguntas[i].opciones.length; j++) {
+            if (!(this.examen.preguntas[i].opciones[j].res == false || this.examen.preguntas[i].opciones[j].res == undefined)) {
+              this.respuestas[this.respuestas.length - 1].respuesta.push(this.examen.preguntas[i].opciones[j].respuesta);
+              if (this.examen.preguntas[i].opciones[j].es_correcta == 1 && this.examen.preguntas[i].opciones[j].res == true) {
+                internas++;
+              } else {
+                internas = this.examen.preguntas[i].num_correctas + 1;
+                break;
+              }
+            }
+          }
+          if (internas == this.examen.preguntas[i].num_correctas) {
+            correctas++;
+          }
+          break;
+      }
+    }
+    console.log(this.respuestas)
+    return Math.round((correctas / this.examen.preguntas.length) * 100);
   }
 
   siguiente(): void {
