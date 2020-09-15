2c257a6713c40815252cc78c7a7228dda8006d9f
Carlos <manuelg.e99@gmail.com>
2020-09-15 16:52:34 -0600

    Se creó funcionalidad de Verificar que exista la clase solicitada

--- backend/endpoints/unirse_clase.js
+++ backend/endpoints/unirse_clase.js
@@ -1,9 +1,15 @@
 module.exports=(app,connection)=>{
     app.post('/unirse_a_clase',(req,res)=>{
-        connection.query(`select * from CLASE`, function (err, rows, fields) {
+        var { cod_clase } = req.body;
+        connection.query(`select * from CLASE where cod_acceso= "${cod_clase}" `, function (err, rows, fields) {
             if (!err) {
-                var convertedJSON = JSON.parse('{"statusCode1":200, "cuerpo":'+ JSON.stringify(rows) +'}');
+                if(JSON.stringify(rows)=="[]"){
+                    var convertedJSON = JSON.parse('{"statusCode1":200, "ExisteClase": false, "cuerpo":'+ JSON.stringify(rows) +'}');
+                }else{
+                    var convertedJSON = JSON.parse('{"statusCode1":200, "ExisteClase": true, "cuerpo":'+ JSON.stringify(rows) +'}');
+                }
                 res.status(200).send(convertedJSON);
+                
             }
             else {
                 res.status(404).send(err);
