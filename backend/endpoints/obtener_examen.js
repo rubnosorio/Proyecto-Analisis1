06da12b0b2f6533fd0bb54259adc0bb8d373a04e
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-09-15 11:52:52 -0600

    Se agrego la prueba que verifica el estado al obtener el examen

--- backend/endpoints/obtener_examen.js
+++ backend/endpoints/obtener_examen.js
@@ -1,12 +1,3 @@
 module.exports = (app,connection) => {
-    app.get('/get_examenes', async (req,res) => {
-        connection.query(`select * from EXAMEN`,function (err, rows, fields) {
-            if (!err) {
-                res.send(rows)
-            }
-            else {
-                throw err;
-            }
-        });
-    });
+    
 }
---
+++ backend/spec/obtenerExamenSpec.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const server = require('../index');
+
+const endpoint = 'http://localhost:3000/get_examenes';
+
+describe('obtiene los examenes de X estudiante', function () {
+    var data = {
+        id_usuario: 1,
+        id_clase: 1
+    }
+    it('get_examenes 200', function (done) {
+        request.post(endpoint, { json: true, body: data }, function (error, response) {
+            expect(response.statusCode).toEqual(200);
+            done();
+        });
+    });
+});
