e6e099293cf34545206147c9a2c02abe4bcbe815
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-15 12:57:43 -0600

    Se agregó codigo en el endpoint de lista_actividades (prueba aceptada)

--- backend/endpoints/lista_actividades.js
+++ backend/endpoints/lista_actividades.js
@@ -1,5 +1,5 @@
 module.exports = (app,connection) => {
-    app.get('/lista_actividades', async (req,res) => {
-        res.statusCode(200).send(true);
+    app.post('/lista_actividades', async (req,res) => {
+        res.status(200).send(true);
     });
 }
