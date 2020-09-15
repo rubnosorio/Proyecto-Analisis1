1f8b27818de5acabdfc9d8bdc918403073816922
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-14 18:34:28 -0600

    Se actualizo la prueba para nota de estudiantes

--- backend/spec/notaEstudiantesSpec.js
+++ backend/spec/notaEstudiantesSpec.js
@@ -16,19 +16,19 @@ describe('nota de estudiantes', function () {
       jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
     });
     it('debe devolver estado 200', function (done) {
-        request.get(endpoint, { json: true, body: clase }, function (error, response) {
+        request.post(endpoint, { json: true, body: clase }, function (error, response) {
             expect(response.statusCode).toEqual(200);
             done();
         });
     });
     it('debe devolver una lista no vacía o vacía',function(done){
-        request.get(endpoint, { json: true, body: clase }, function (error, response) {
+        request.post(endpoint, { json: true, body: clase }, function (error, response) {
             expect(response.body.lista_notas.length).toBeGreaterThanOrEqual(0);
             done();
         });
     });
     it('debe ser mayor o igual a 0 la nota de tareas',function(done){
-        request.get(endpoint, { json: true, body: clase }, function (error, response) {
+        request.post(endpoint, { json: true, body: clase }, function (error, response) {
             if(response.body.lista_notas.length>0){
                 var nota_tarea=0;
                 for(let i=0;i<response.body.lista_notas[0].lista_tareas.length;i++){
