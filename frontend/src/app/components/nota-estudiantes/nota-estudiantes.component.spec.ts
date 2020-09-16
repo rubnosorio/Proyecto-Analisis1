cddc5de6cdee5b96dc904040ce03d9d0bebaa5f5
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-15 23:07:13 -0600

    Se agregó la función para obtener los datos a mostrar en la tabla de nota de estudiantes

--- frontend/src/app/components/nota-estudiantes/nota-estudiantes.component.spec.ts
+++ frontend/src/app/components/nota-estudiantes/nota-estudiantes.component.spec.ts
@@ -3,11 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotaEstudiantesComponent } from './nota-estudiantes.component';
 import { NotaEstudiante } from "../../models/nota-estudiante";
 import { ListaActividades } from "../../models/lista-actividades";
+import { NotaTarea } from '../../models/nota-tarea';
+import { NotaExamen } from '../../models/nota-examen';
 
 fdescribe('NotaEstudiantesComponent', () => {
   let component: NotaEstudiantesComponent;
   let fixture: ComponentFixture<NotaEstudiantesComponent>;
   var lista_actividades:ListaActividades[]=[];
+  var lista_notas:NotaEstudiante[]=[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -33,6 +36,30 @@ fdescribe('NotaEstudiantesComponent', () => {
     lista_actividades.push(actividad4);
     lista_actividades.push(actividad5);
     lista_actividades.push(actividad6);
+    var estudiante1=new NotaEstudiante(1,'Erick','Lemus',[],[]);
+    var estudiante2=new NotaEstudiante(2,'Carlos','Garcia',[],[]);
+    var tarea1e1=new NotaTarea(1,'tarea 1',5);
+    var tarea2e1=new NotaTarea(2,'tarea 2',4);
+    var tarea3e1=new NotaTarea(3,'tarea 3',6);
+    var tarea1e2=new NotaTarea(1,'tarea 1',5);
+    var tarea2e2=new NotaTarea(2,'tarea 2',4);
+    var tarea3e2=new NotaTarea(3,'tarea 3',6);
+    var examen1e1=new NotaExamen(1,'primer parcial',8);
+    var examen2e1=new NotaExamen(2,'segundo parcial',10);
+    var examen1e2=new NotaExamen(1,'primer parcial',8);
+    var examen2e2=new NotaExamen(2,'segundo parcial',10);
+    estudiante1.lista_tareas.push(tarea1e1);
+    estudiante1.lista_tareas.push(tarea2e1);
+    estudiante1.lista_tareas.push(tarea3e1);
+    estudiante2.lista_tareas.push(tarea1e2);
+    estudiante2.lista_tareas.push(tarea2e2);
+    estudiante2.lista_tareas.push(tarea3e2);
+    estudiante1.lista_examenes.push(examen1e1);
+    estudiante1.lista_examenes.push(examen2e1);
+    estudiante2.lista_examenes.push(examen1e2);
+    estudiante2.lista_examenes.push(examen2e2);
+    lista_notas.push(estudiante1);
+    lista_notas.push(estudiante2);
   });
 
   it('should create', () => {
@@ -52,6 +79,7 @@ fdescribe('NotaEstudiantesComponent', () => {
   });
   
   it('Deberia devolver true si la funcion fue exitosa',()=>{
+    console.log(lista_notas);
     expect(component.obtenerColumnas(lista_actividades)).toBeTruthy();
   });
 
