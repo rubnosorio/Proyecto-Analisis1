a2ccd73bc0ee8c26fe83fa932dae010d0b642628
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-15 18:34:41 -0600

    Se actualizo el modelo y prueba de nota-estudiante (prueba aceptada)

--- frontend/src/app/models/nota-estudiante.spec.ts
+++ frontend/src/app/models/nota-estudiante.spec.ts
@@ -2,6 +2,6 @@ import { NotaEstudiante } from './nota-estudiante';
 
 describe('NotaEstudiante', () => {
   it('Deberia crear un estudiante con sus notas', () => {
-    expect(new NotaEstudiante(1,'Erick','Lemus',[])).toBeTruthy();
+    expect(new NotaEstudiante(1,'Erick','Lemus',[],[])).toBeTruthy();
   });
 });
--- frontend/src/app/models/nota-estudiante.ts
+++ frontend/src/app/models/nota-estudiante.ts
@@ -1,15 +1,18 @@
 import { NotaTarea } from './nota-tarea'
+import { NotaExamen } from './nota-examen'
 
 export class NotaEstudiante {
     id_estudiante:number
     nombres:string
     apellidos:string
     lista_tarea: NotaTarea[]
+    lista_examen:NotaExamen[]
 
-    constructor(_id_estudiante:number, _nombres:string, _apellidos:string, _lista_tarea:NotaTarea[]){
+    constructor(_id_estudiante:number, _nombres:string, _apellidos:string, _lista_tarea:NotaTarea[], _lista_examen:NotaExamen[]){
         this.id_estudiante=_id_estudiante;
         this.nombres=_nombres;
         this.apellidos=_apellidos;
         this.lista_tarea=_lista_tarea;
+        this.lista_examen=_lista_examen;
     }
 }
