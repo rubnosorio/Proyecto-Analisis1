056171e22992fa74945dc19ae9f57445d862792c
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-15 19:20:34 -0600

    Se actualizaron los campos de lista_tarea a lista_tareas y lista_examen a lista_examenes

--- frontend/src/app/models/nota-estudiante.ts
+++ frontend/src/app/models/nota-estudiante.ts
@@ -5,14 +5,14 @@ export class NotaEstudiante {
     id_estudiante:number
     nombres:string
     apellidos:string
-    lista_tarea: NotaTarea[]
-    lista_examen:NotaExamen[]
+    lista_tareas: NotaTarea[]
+    lista_examenes:NotaExamen[]
 
     constructor(_id_estudiante:number, _nombres:string, _apellidos:string, _lista_tarea:NotaTarea[], _lista_examen:NotaExamen[]){
         this.id_estudiante=_id_estudiante;
         this.nombres=_nombres;
         this.apellidos=_apellidos;
-        this.lista_tarea=_lista_tarea;
-        this.lista_examen=_lista_examen;
+        this.lista_tareas=_lista_tarea;
+        this.lista_examenes=_lista_examen;
     }
 }
