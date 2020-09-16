32055c153df160c1c2b1f22c15d6690b1691650f
Carlos <manuelg.e99@gmail.com>
2020-09-15 19:11:43 -0600

    Se Agregó prueba si se agregó el usuario al curso

--- backend/spec/UnirseClaseSpec.js
+++ backend/spec/UnirseClaseSpec.js
@@ -6,7 +6,7 @@ var respues;
 
 fdescribe('unirse_a_clase', function () {
     beforeAll((done) => {
-        request.post(endpoint,{json: true, body:{"cod_clase":"abcdefghi"}}, function (error, response) {
+        request.post(endpoint,{json: true, body:{"cod_clase":"abcdefghi","id_usuario": 1}}, function (error, response) {
             respues = JSON.stringify(response);
             done();
         });
@@ -17,11 +17,12 @@ fdescribe('unirse_a_clase', function () {
             expect(JSON.parse(respues).body.statusCode1).toEqual(200);
     });
     
-    
     it('Verificar que la clase exista', function () {
         expect(JSON.parse(respues).body.ExisteClase).toBeTruthy();
     });
   
-
+    it('Verificar que Haya registrado la unión al curso', function () {
+        expect(JSON.parse(respues).body.statusCode2).toEqual(200);
+    });
     
 });
