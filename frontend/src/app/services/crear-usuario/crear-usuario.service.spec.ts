3b6fb9dff3bac3adff3344cf65bb2a2fcb271f4c
Henry Galvez <soulofhenry@gmail.com>
2020-09-22 13:49:07 -0600

    prueba: posteo a servidor con mock (exito)

--- frontend/src/app/services/crear-usuario/crear-usuario.service.spec.ts
+++ frontend/src/app/services/crear-usuario/crear-usuario.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CrearUsuarioService } from './crear-usuario.service';
+import { Usuario } from '../../models/usuario'
 
 fdescribe('Given que se quiere mandar usuarios al servidor', () => {
   let service: CrearUsuarioService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,13 +14,14 @@ fdescribe('Given que se quiere mandar usuarios al servidor', () => {
       ]
     });
     service = TestBed.inject(CrearUsuarioService);
+    httpMock = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('when la URL existe', () => {
+  it('When la URL existe', () => {
     let base64 = service.getbaseURL()
     expect(base64).toBeTruthy();
   });
@@ -26,5 +29,18 @@ fdescribe('Given que se quiere mandar usuarios al servidor', () => {
   it('And el metodo para postear el usuario existe', () => {
     let res = service.postUser
     expect(res).toBeTruthy();
+  });
+
+  it('Then el usuario es posteado con exito', () => {
+    const mockRes = 200;
+    var res = 0
+    service.postUser(new Usuario(1,"prueba","prueba","prueba","prueba","prueba@prueba.com"))
+    .subscribe(resAPI =>{
+      res = resAPI
+    })
+    const req = httpMock.expectOne(service.getbaseURL()+'crear-usuario');
+    req.flush(mockRes);
+    console.log(req)
+    expect(res).toEqual(200);
   });
 });
