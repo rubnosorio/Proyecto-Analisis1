b83ddfe40a9617604a58d101fbeabf73b2f6dcc9
René Corona <3114327190409@ingenieria.usac.edu.gt>
2020-10-21 23:00:59 -0600

    Se agrego ChromeHeadless a las pruebas

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,8 +25,32 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [
+      "ChromeHeadlessNoSandbox"
+    ],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          // required to run without privileges in Docker
+          "--disable-web-security",
+          "--disable-gpu",
+          "--remote-debugging-port=9222"
+        ]
+      }
+    },
     singleRun: false,
+    junitReporter: {
+      outputDir: 'test-reports',
+      outputFile: 'junit-report.xml',
+      suite: '',
+      useBrowserName: false,
+      nameFormatter: undefined,
+      classNameFormatter: undefined,
+      properties: {
+      }
+    },
     restartOnFileChange: true
   });
 };
