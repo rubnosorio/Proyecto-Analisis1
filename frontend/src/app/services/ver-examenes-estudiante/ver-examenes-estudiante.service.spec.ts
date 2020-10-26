e36a131eb11c22be98d9946041bb4f047a04b916
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-10-26 02:57:52 -0600

    Se agregaron pruebas BDD par vistas de clases y examenes del estudiante

--- frontend/src/app/services/ver-examenes-estudiante/ver-examenes-estudiante.service.spec.ts
+++ frontend/src/app/services/ver-examenes-estudiante/ver-examenes-estudiante.service.spec.ts
@@ -1,16 +1,65 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { VerExamenesEstudianteService } from './ver-examenes-estudiante.service';
 
 describe('VerExamenesEstudianteService', () => {
   let service: VerExamenesEstudianteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [VerExamenesEstudianteService, HttpClient]
+    });
     service = TestBed.inject(VerExamenesEstudianteService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Dado que una clase tiene varios examenes', () => {
+    it('Se obtienen correctamente los examenes', (done) => {
+        service.getMockClases().subscribe((res:any[]) => {
+          let clasefake = {
+            id_clase: res[0].id_clase,
+            nombre_clase: res[0].nombre_clase,
+            cod_acceso: res[0].cod_acceso,
+            id_usuario: res[0].id_usuario
+          }
+          service.getMockExamenes().subscribe((res:any[]) => {
+              let examenes=0;
+              for(let i=0; i<res.length; i++){
+                if(clasefake.id_clase==res[i].id_clase){
+                  examenes++;
+                }
+              }
+              expect(examenes).toBeGreaterThan(0);
+              done();
+          });
+        })
+    });
+  });
+
+  describe('Dado que una clase no tiene examenes', () => {
+    it('No se logra obtener ningun examen', (done) => {
+        service.getMockClases().subscribe((res:any[]) => {
+          let clasefake = {
+            id_clase: res[1].id_clase,
+            nombre_clase: res[1].nombre_clase,
+            cod_acceso: res[1].cod_acceso,
+            id_usuario: res[1].id_usuario
+          }
+          service.getMockExamenes().subscribe((res:any[]) => {
+              let examenes=0;
+              for(let i=0; i<res.length; i++){
+                if(clasefake.id_clase==res[i].id_clase){
+                  examenes++;
+                }
+              }
+              expect(examenes).toEqual(0);
+              done();
+          });
+        })
+    });
   });
 });
--- frontend/src/app/services/ver-examenes-estudiante/ver-examenes-estudiante.service.ts
+++ frontend/src/app/services/ver-examenes-estudiante/ver-examenes-estudiante.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { baseURL } from '../shared/baseURL';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,14 @@ import { baseURL } from '../shared/baseURL';
 export class VerExamenesEstudianteService {
 
   constructor(private http:HttpClient) { }
+
+  getMockClases():Observable<any[]> {
+    return this.http.get<any[]>('assets/fakedb/ClaseFakeTable.json');
+  }
+
+  getMockExamenes():Observable<any[]> {
+    return this.http.get<any[]>('assets/fakedb/ExamenesFake.json');
+  }
 
   get_examenes(clase:any){
     return this.http.post(baseURL+'ver_examenes_estudiante',clase);
