3d7e2c3d6539b6c6404fdb99cf73129c0d6e05c4
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-10-26 19:42:38 -0600

    Se agregaron pruebas BDD para verificar fecha limite de entrega

--- frontend/src/app/components/select-tarea/select-tarea.component.spec.ts
+++ frontend/src/app/components/select-tarea/select-tarea.component.spec.ts
@@ -2,15 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectTareaComponent } from './select-tarea.component';
 import { SelectTareaService } from "../../services/select-tarea/select-tarea.service";
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-describe('SelectTareaComponent', () => {
+fdescribe('SelectTareaComponent', () => {
   let component: SelectTareaComponent;
   let fixture: ComponentFixture<SelectTareaComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ SelectTareaComponent ],
-      providers:[SelectTareaService]
+      providers:[SelectTareaService,HttpClient]
     })
     .compileComponents();
   }));
@@ -24,4 +26,23 @@ describe('SelectTareaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Dado una tarea con una fecha limite de entrega',()=>{
+    describe('Cuando entro a esa tarea y la fecha limite no se ha cumplido',()=>{
+      let fecha='2020-11-20';
+      it('Entonces si podre entregarla',()=>{
+        expect(component.verificar_fecha(fecha)).toEqual(1);
+      });
+    });
+  });
+
+  describe('Dado una tarea con una fecha limite de entrega',()=>{
+    describe('Cuando entro a esa tarea y la fecha limite ya se ha cumplido',()=>{
+      let fecha='2020-10-20';
+      it('Entonces no podre entregarla',()=>{
+        expect(component.verificar_fecha(fecha)).toEqual(0);
+      });
+    });
+  });
+
 });
